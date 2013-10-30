aee84947a364cf5d1cde59c70eb0bc69e2e5ee2d
Jan Grzybowski <grzybowskij@student.mini.pw.edu.pl>
2013-10-30 12:04:32 +0100

    Dorobiona klasa Usera

--- AF Models/User.cs
+++ AF Models/User.cs
@@ -8,5 +8,9 @@ namespace AF_Models
 {
     class User
     {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
     }
 }
