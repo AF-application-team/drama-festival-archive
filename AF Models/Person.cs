fb47920169a0c8158f75d65f9aa8aa8378a86402
Jan Grzybowski <grzybowskij@student.mini.pw.edu.pl>
2013-10-30 12:02:01 +0100

    Dodana klasa Person i zaczęta User Plus poprawki w DB Plus podpięty NuGet

---
+++ AF Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AF_Models
+{
+    class Person 
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool Sex { get; set; }
+        public int Year { get; set; }
+        public DateTime EditDate { get; set; }
+        public int EditedBy { get; set; }
+        public string Profile { get; set; } 
+    }
+}
