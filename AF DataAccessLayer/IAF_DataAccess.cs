c1eedfef77d3df5cab8a36262daad134f62a71f1
Jan Grzybowski <grzybowskij@student.mini.pw.edu.pl>
2013-11-01 00:44:59 +0100

    Klasa interfejsu DALa

---
+++ AF DataAccessLayer/IAF_DataAccess.cs
@@ -0,0 +1,20 @@
+﻿using AF_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AF_DataAccessLayer
+{
+    public interface IAF_DataAccess
+    {
+        #region Person
+        int AddPerson(Person newPerson);
+        void RemovePerson(int id);
+        Person GetPerson(int id);
+        List<Person> GetPeoplePaged(int pageNr, int pageAmount);
+        void UpdatePerson(Person updateData);
+        #endregion
+    }
+}
