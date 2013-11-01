9888e9724bb73e9dbdb1a04bb6306f4668992fce
Anna Witwicka <witwickaa@student.mini.pw.edu.pl>
2013-11-01 23:45:17 +0100

    Poprawione dalsze literowki

--- AF Models/RelationPersonPlayRole.cs
+++ AF Models/RelationPersonPlayRole.cs
@@ -8,7 +8,7 @@ namespace AF_Models
 {
     public class RelationPersonPlayRole
     {
-        public int RelationPresonPlayRoleId { get; set; }
+        public int RelationPersonPlayRoleId { get; set; }
         public int PersonId { get; set; }
         public int PlayId { get; set; }
         public string Role { get; set; }
