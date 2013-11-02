c79df9010dbb9f82c0f205c6edd3b508f106c036
Jan Grzybowski <grzybowskij@student.mini.pw.edu.pl>
2013-11-02 23:39:03 +0100

    Okno dodawania nagrody przedstawieniu

---
+++ AF BusinessLogic/AF_Logic.cs
@@ -0,0 +1,14 @@
+﻿using AF_DataAccessLayer
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AF_BusinessLogic
+{
+    class AF_Logic
+    {
+        //IAF_DataAccess DataAccess = new ;
+    }
+}
