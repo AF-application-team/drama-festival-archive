7cdb5f2ec9d8f14fb6d39b3e42df7448621c6e66
Jan Grzybowski <grzybowskij@student.mini.pw.edu.pl>
2013-11-06 03:55:12 +0100

    Dodanie klas kryterów wyszukiwania

---
+++ AF Searching Criteria/SearchingCriteria.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AF_Searching_Criteria
+{
+    public class PeopleSearchingCriteria
+    {
+        public int? JobId { get; set; }
+        public int? PositionId { get; set; }
+        public int? CategoryId { get; set; }
+        public int? Year { get; set; }
+        public char? Profile { get; set; }
+    }
+
+    public class PlaysSearchingCriteria
+    {
+        public int? CategoryId { get; set; }
+        public int? FestivalId { get; set; }
+    }
+
+    public class AwardsSearchingCriteria
+    {
+        public int? CategoryId { get; set; }
+        public int? FestivalId { get; set; }
+    }
+}
