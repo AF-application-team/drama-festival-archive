475d556c2be556b574041ac388b7dbf4772062df
Jan Grzybowski <grzybowskij@student.mini.pw.edu.pl>
2013-11-11 17:37:53 +0100

    Copy constructo i Equals w Category

--- AF Models/Category.cs
+++ AF Models/Category.cs
@@ -24,5 +24,35 @@ namespace AF_Models
 
         [ForeignKey("EditedBy")]
         public virtual User Editor { get; set; }
+
+        public Category() { }
+        public Category(Category category)
+        {
+            CategoryId = category.CategoryId;
+            Title = category.Title;
+            Group = category.Group;
+            Order = category.Order;
+            EditDate = category.EditDate;
+            EditedBy = category.EditedBy;
+        }
+
+        public override bool Equals(object obj)
+        {
+            // If parameter is null return false.
+            if (obj == null)
+            {
+                return false;
+            }
+
+            // If parameter cannot be cast to Point return false.
+            var c = obj as Category;
+            if ((System.Object)c == null)
+            {
+                return false;
+            }
+
+            // Return true if the fields match:
+            return (CategoryId == c.CategoryId) && (Title == c.Title) && (Group == c.Group) && (Order == c.Order);
+        }
     }
 }
