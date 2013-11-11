5bab35a19652ebb82d57a1567187f70229d82d5c
Jan Grzybowski <grzybowskij@student.mini.pw.edu.pl>
2013-11-11 19:54:35 +0100

    Naprzemienne kolorowanie List w konfiguracji

---
+++ AF Desktop Application/ColorConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace AF_Desktop_Application
+{
+    class ColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int group = (int) value;
+            if (group%2==1) 
+                return new SolidColorBrush(Colors.Beige);
+            return new SolidColorBrush(Colors.White);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
