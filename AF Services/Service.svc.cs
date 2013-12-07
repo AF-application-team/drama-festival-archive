0a66cbc571bf71ff70129edd5756ca74449263d5
Jan Grzybowski <grzybowskij@student.mini.pw.edu.pl>
2013-12-07 21:09:03 +0100

    Dodanie IAFService UWAGA WCF nie będzie się buildował!)

--- AF Services/Service.svc.cs
+++ AF Services/Service.svc.cs
@@ -10,7 +10,7 @@ namespace AF_Services
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service : IService
+    public class AFService : IAFService
     {
         public string GetData(int value)
         {
---
+++ AF.Common/Services/IAFService.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using AF_Models;
+
+namespace AF_Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IAFService
+    {
+        #region Award
+        Award AddAward(int playId, int festivalId, int categoryId, int userId);
+        //Void RemoveAward(int id);
+        Award UpdateAward(Award updateData);
+        Award GetAward(int id);
+        //List<AwardDTO> GetAwardsPaged(int pageNr, int pageAmount);
+        List<AwardDTO> SearchAwards(AwardsSearchingCriteria criteria, int pageNr, int pageAmount);
+        //<List<Award> GetAllAwards();
+        #endregion
+        #region Category
+        Category AddCategory(string title, int group, int order, int userId);
+        //RemoveCategory(int id);
+        Category UpdateCategory(Category updateData);
+        Category GetCategory(int id);
+        //List<Category> GetCategoriesPaged(int pageNr, int pageAmount);
+        List<Category> GetAllCategories();
+        #endregion
+        #region Festival
+        Festival AddFestival(Festival newFestival);
+        //RemoveFestival(int id);
+        Festival UpdateFestival(Festival updateData);
+        Festival GetFestival(int id);
+        List<Festival> GetFestivalsPaged(int pageNr, int pageAmount);
+        //List<Festival> GetAllFestivals();
+        int CountFestivals();
+        #endregion
+        #region Job
+        Job AddJob(string title, int userId);
+        //RemoveJob(int id);
+        Job UpdateJob(Job updateData);
+        Job GetJob(int id);
+        //List<Job> GetJobsPaged(int pageNr, int pageAmount);
+        List<Job> GetAllJobs();
+        #endregion
+        #region News
+        //AddNews(News newNews);
+        //RemoveNews(int id);
+        //UpdateNews(News updateData);
+        //News GetNews(int id);
+        //List<News> GetNewsPaged(int pageNr, int pageAmount);
+        //List<News> GetAllNews();
+        #endregion
+        #region Person
+        //Person AddPerson(Person newPerson);
+        //RemovePerson(int id);
+        //Person UpdatePerson(Person updateData);
+        //Person GetPerson(int id);
+        //List<PersonDTO> GetPeoplePaged(int pageNr, int pageAmount);
+        //List<Person> GetAllPeople();
+        #endregion
+        #region Play
+        Play AddPlay(Play newPlay);
+        //RemovePlay(int id);
+        Play UpdatePlay(Play updateData);
+        Play GetPlay(int id);
+        //List<PlayDTO> GetPlaysPaged(int pageNr, int pageAmount);
+        //List<Play> GetAllPlays();
+        List<Play> SearchPlays(PlaysSearchingCriteria criteria, int pageNr, int pageAmount);
+        #endregion
+        #region Position
+        Position AddPosition(string title, int section, int order, int userId);
+        //RemovePosition(int id);
+        Position UpdatePosition(Position updateData);
+        Position GetPosition(int id);
+        //List<Position> GetPositionsPaged(int pageNr, int pageAmount);
+        List<Position> GetAllPositions();
+        #endregion
+        #region RelationFestivalPersonPosition
+        AddRelationFestivalPersonPosition(RelationFestivalPersonPosition newRelationFestivalPersonPosition);
+        RemoveRelationFestivalPersonPosition(int id);
+        UpdateRelationFestivalPersonPosition(RelationFestivalPersonPosition updateData);
+        RelationFestivalPersonPosition> GetRelationFestivalPersonPosition(int id);
+        List<RelationFestivalPersonPosition> GetRelationFestivalPersonPositionPaged(int pageNr, int pageAmount);
+        List<RelationFestivalPersonPosition> GetAllRelationFestivalPersonPosition();
+        #endregion
+        #region RelationPersonAward
+        AddRelationPersonAward(RelationPersonAward newRelationPersonAward);
+        RemoveRelationPersonAward(int id);
+        UpdateRelationPersonAward(RelationPersonAward updateData);
+        RelationPersonAward> GetRelationPersonAward(int id);
+        List<RelationPersonAward> GetRelationPersonAwardPaged(int pageNr, int pageAmount);
+        List<RelationPersonAward> GetAllRelationPersonAward();
+        #endregion
+        #region RelationPersonPlayJob
+        AddRelationPersonPlayJob(RelationPersonPlayJob newRelationPersonPlayJob);
+        RemoveRelationPersonPlayJob(int id);
+        UpdateRelationPersonPlayJob(RelationPersonPlayJob updateData);
+        RelationPersonPlayJob> GetRelationPersonPlayJob(int id);
+        List<RelationPersonPlayJob> GetRelationPersonPlayJobPaged(int pageNr, int pageAmount);
+        List<RelationPersonPlayJob> GetAllRelationPersonPlayJob();
+        #endregion
+        #region RelationPersonPlayRole
+        AddRelationPersonPlayRole(RelationPersonPlayRole newRelationPersonPlayRole);
+        RemoveRelationPersonPlayRole(int id);
+        UpdateRelationPersonPlayRole(RelationPersonPlayRole updateData);
+        RelationPersonPlayRole> GetRelationPersonPlayRole(int id);
+        List<RelationPersonPlayRole> GetRelationPersonPlayRolePaged(int pageNr, int pageAmount);
+        List<RelationPersonPlayRole> GetAllRelationPersonPlayRole();
+        #endregion
+        #region User
+        AddUser(User newUser);
+        RemoveUser(int id);
+        UpdateUser(User updateData);
+        User> GetUser(int id);
+        List<User> GetUserPaged(int pageNr, int pageAmount);
+        List<User> GetAllUsers();
+        #endregion
+
+
+        //[OperationContract]
+        //string GetData(int value);
+
+        //[OperationContract]
+        //CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
