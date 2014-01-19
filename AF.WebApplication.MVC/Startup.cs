8a39a06b7cdf1a8c5b46f99baee7d366e61b553d
Jan Grzybowski <grzybowskij@student.mini.pw.edu.pl>
2014-01-19 18:03:48 +0100

    Przerobienie na MVC 5

---
+++ AF.WebApplication.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AF.WebApplication.MVC.Startup))]
+namespace AF.WebApplication.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
